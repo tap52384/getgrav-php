105439b416f3395dfc4d1d9a1750f4d08a14a59a
Z. Patrick Lewis <patrick_lewis@unc.edu>
2018-10-01 12:58:57 -0400

    fixes re-commenting commented out lins in ext-opcache.ini

--- install-php.sh
+++ install-php.sh
@@ -204,13 +204,13 @@ DOCUMENTROOT_LINE_NUM=$(grep -n '^DocumentRoot.*' /usr/local/etc/httpd/httpd.con
 
 # TODO: Add a handler for all files that match *.php as application/x-httpd-php
 
-# TODO: Turn off opcache for PHP 5.6 in order for it to work properly
+# Turn off opcache for PHP 5.6 in order for it to work properly
 # https://discourse.brew.sh/t/segmentation-fault-on-mojave-http24-php56/3043/7
 # Comments out all lines using a semi-colon
 # https://stackoverflow.com/a/2099478/1620794
 #replaceline /usr/local/etc/php/5.6/conf.d/ext-opcache.ini '^opcache.enabled=.*' 'opcache.enabled=0' 'Disabling opcache on PHP 5.6...'
 echo "Disabling opcache on PHP 5.6..."
-sed -i '' -e 's/^/;/' /usr/local/etc/php/5.6/conf.d/ext-opcache.ini
+sed -i '' -e '/^;/! s/^/;/' /usr/local/etc/php/5.6/conf.d/ext-opcache.ini
 
 # Add config files for apcu, xdebug, and yaml PHP packages
 # Replace the specific line in a text file with a string
